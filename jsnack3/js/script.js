92ccae2018dd85f23d221ec63339ad6e8f499584
arianna-cosimini <arianna.cosimini@gmail.com>
2024-02-06 13:55:51 +0100

    fix snack 3 : inserita condizione

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,20 +1,28 @@
 const nameList1 = ["Arianna", "Nicolò", "Emanuele"];
 console.log(nameList1)
 
-const namelist2 = ["Cinzia", "Paolo", "Celeste", "Luca", "Marco", "Sofia"];
+const namelist2 = ["Cinzia", "Paolo","Luca","Celeste","Marco","Sofia"];
 console.log(namelist2)
 
 let userQuestion;
 let result;
 
 do {
+
     userQuestion=prompt("Inserisci un nome");
 
-    
+        if(nameList1.length < namelist2.length){
+            
         nameList1.push(userQuestion);
         console.log(nameList1)
     
+        }else if(namelist2.length < nameList1.length){
+            namelist2.push(userQuestion)
+        }
+        
+
     
-}while (nameList1.length != namelist2.length){
+    
+}while (nameList1.length !== namelist2.length){
     result= document.querySelector("p").innerHTML = `${nameList1} <br> ha la stessa lunghezza di: <br> ${namelist2}`;
 }
